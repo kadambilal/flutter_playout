0201a61cf3db63ddf8eba2b86975583bc360bb7a
Muhammed OKUR <okurokur2014@gmail.com>
2022-04-14 10:41:47 +0300

    Create FlutterPlayoutPlugin.kt

---
+++ android/src/main/kotlin/tv/mta/flutter_playout/FlutterPlayoutPlugin.kt
@@ -0,0 +1,13 @@
+package tv.mta.flutter_playout
+
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+
+class FlutterPlayoutPlugin {
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+
+    }
+  }
+}
