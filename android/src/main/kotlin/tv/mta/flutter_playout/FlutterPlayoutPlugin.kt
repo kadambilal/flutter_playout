3dd8b6b3a072e27657968a74b8d94abdc51df462
Muhammed OKUR <okurokur2014@gmail.com>
2022-04-14 11:00:09 +0300

    updated

--- android/src/main/kotlin/tv/mta/flutter_playout/FlutterPlayoutPlugin.kt
+++ android/src/main/kotlin/tv/mta/flutter_playout/FlutterPlayoutPlugin.kt
@@ -1,13 +1,7 @@
 package tv.mta.flutter_playout
 
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 
 class FlutterPlayoutPlugin {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
 
-    }
-  }
 }
